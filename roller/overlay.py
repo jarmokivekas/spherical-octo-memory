b37c69a38780ef5f29aedcdd65e8643f6494f83a
Jarmo Kivekas <jarmo.kivekas@gmail.com>
2024-10-26 22:41:42 +0300

    add overlay line plot class

--- roller/overlay.py
+++ roller/overlay.py
@@ -49,3 +49,71 @@ class Overlay:
     @staticmethod
     def get_middle_alignment_offset(text_surface, screen_surface):
         return (screen_surface.get_height() / 2)
+
+
+
+import pygame
+
+class LinePlot:
+    def __init__(self, surface, data, rect, line_color=(255, 165, 0), background_color=(0, 0, 0)):
+        """
+        Initialize the LinePlot class.
+
+        Args:
+            surface: The Pygame surface to draw the plot on.
+            data: List of (x, y) tuples representing the data points.
+            rect: A pygame.Rect defining the area of the plot on the surface.
+            line_color: The color of the line plot (default is orange).
+            background_color: The background color of the plot (default is black).
+        """
+        self.surface = surface
+        self.data = data
+        self.rect = rect
+        self.line_color = line_color
+        self.background_color = background_color
+        self.idx = 0
+
+    def add_data(self, point):
+        """Uses a round-robin data buffer to add a new datapoint, replacing the oldest one."""
+        self.idx = (self.idx + 1) % len(data)
+        assert(self.idx < len(data))  
+
+        # Update the minimum and maximum limits of the data, used to scale plotted values
+        x_min = point.x if point.x < x_min else x_min
+        x_max = point.x if point.x > x_max else x_max
+        y_min = point.y if point.y < y_min else y_min
+        y_max = point.y if point.y > y_max else y_max
+
+        data[self.idx] = point
+
+    def update_plot()
+
+        pygame.draw.line(self.surface, self.line_color, scale(self.data[i]), scale(self.data[i+1]), 1)
+
+
+    def scale(self, point, x_min, x_max, y_min, y_max):
+        """Scale the (x, y) data point to fit within the plot rect."""
+        x, y = point
+        x_scaled = self.rect.left + (x - x_min) / (x_max - x_min) * self.rect.width
+        y_scaled = self.rect.bottom - (y - y_min) / (y_max - y_min) * self.rect.height
+        return (x_scaled, y_scaled)
+
+    def draw(self):
+        """Draw the line plot on the given surface."""
+        # Fill the background of the plot
+        pygame.draw.rect(self.surface, self.background_color, self.rect)
+
+        if len(self.data) < 2:
+            return  # Not enough data to draw a line
+
+        # Scale the data points to fit within the plot area
+        x_vals = [point[0] for point in self.data]
+        y_vals = [point[1] for point in self.data]
+
+        x_min, x_max = min(x_vals), max(x_vals)
+        y_min, y_max = min(y_vals), max(y_vals)
+
+        x_vals = self.scale(x)
+        # Draw the lines connecting the scaled data points
+        for i in range(len(self.data) - 1):
+            pygame.draw.line(self.surface, self.line_color, scale(self.data[i]), scale(self.data[i+1]), 1)
