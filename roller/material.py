a3f9001d2cff2a0bf67d0ef92cd18a4cd7cf67cf
Jarmo Kivekas <jarmo.kivekas@gmail.com>
2024-10-27 13:22:10 +0200

    improve scattering probability calculation using lru cache

--- roller/material.py
+++ roller/material.py
@@ -3,6 +3,7 @@ This module defines what materials and properties are represented by different
 color pixels in the world raster
 """
 
+from functools import lru_cache
 import random
 from roller.datatypes import Point
 
@@ -18,8 +19,14 @@ The values is mapped to the green channel of the world raster.
 i.e. the pixel is completely opaque to light.
 255 means the pixel will never scatter."""
 
-def get_scattering_propability(pixel_color: tuple):
-    return (1 - pixel_color[SCATTERMAP] /255) **2
+@lru_cache(maxsize=None)  # maxsize=None means unlimited cache size. limit it if needed
+def get_scattering_probability(scattermap_value: int):
+    """Calculates the laser scattering probability based on the pixel color.
+    Uses caching to avoid recalculating for the same color values (@lru_cache decorator).
+    
+    Caching this result reduced profiling time for 0.460 seconds to < 0.000 seconds during 10 seconds of the gameloop
+    """
+    return (1 - scattermap_value / 255) ** 2
 
 def get_temperature_at(point: Point, world):
     pixel_color = world.surface.get_at((int(point.x),int(point.y)))
@@ -27,6 +34,6 @@ def get_temperature_at(point: Point, world):
     return temperature
 
 def is_light_scattering(pixel_color: tuple):
-    scattering_propability =  get_scattering_propability(pixel_color)
+    scattering_propability =  get_scattering_probability(pixel_color[SCATTERMAP])
     return random.random() < scattering_propability
 
